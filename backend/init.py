96361b0e23ebad4ce87b12ae7763c02450c69c7c
makefu <github@syntax-fehler.de>
2014-08-13 10:30:45 +0200

    rewritten backend

--- backend/init.py
+++ backend/init.py
@@ -3,20 +3,42 @@
 from flask import Flask,Response
 from time import time
 from datetime import datetime,timedelta
+import requests
 import json
 app = Flask(__name__)
 import redis
 redis_host='glados.shack'
 redis_port=6379
 redis_db=0
-sensor_namespace='sensors.motion.{}'
+sensor_namespace='sensors.motion.id.{}'
 # sortierung raum <=> id
 rooms_namespace='sensors.rooms.{}'
+temperature_target='sensors.temp.rooms.lounge.temp'
 timeout=timedelta(seconds=40)
-point_value=timedelta(seconds=50)
-temperature_url='http://heidi:8080/render/?target=sensors.temp.rooms.lounge.temp&format=json'
-
+point_value=timedelta(seconds=60)
+graphite_host="http://heidi:8080"
+simple_sensor_mapping = {
+        "lounge":9846210,
+        "kueche":9584064
+        }
+import logging as log
+log.basicConfig(level=log.DEBUG)
+log.debug("balls")
 r = redis.StrictRedis(host=redis_host, port=redis_port , db=redis_db)
+
+def data_from_graphite(target,addn_params=None):
+    """
+    target is a graphite data namespace like: 'sensors.motion.id.1'
+    addn_params are additional targets which are added onto the graphite query
+    """
+    from urllib.parse import urlencode
+    params= {'target':target,'format':'json'}
+    if addn_params:
+        params.update(addn_params)
+    url='{0}/render/?{1}'.format(graphite_host,urlencode(params))
+    log.debug("url:"+url)
+    #return requests.get(url).json()
+    return requests.get(url).json()
 
 @app.route('/')
 def hello_world():
@@ -24,23 +46,25 @@ def hello_world():
 
 @app.route('/api/sensors')
 def get_all_sensors():
-    q=r.keys(sensor_namespace.format("*"))
-    return json.dumps([ int(i.decode().split(".")[-1]) for i in q ])
+    url="{0}/metrics/find/?query={1}&format=treejson".format(graphite_host,sensor_namespace).format('*')
+
+    return json.dumps([ int(e['id'].split(".")[-1]) \
+            for e in requests.get(url).json() ])
 
 @app.route('/api/sensors/<int:sensor>/activity')
 def add_sensor_data(sensor):
-    nowdate= datetime.now()
+    nowdate=datetime.now()
     timeout=timedelta(seconds=4)
     try:
-        lasttime=datetime.fromtimestamp(float(r.lrange(sensor_namespace.format(sensor),-1,-1)[0]))
+        lasttime = datetime.fromtimestamp( \
+                data_from_graphite( \
+                    sensor_namespace.format(sensor))[0]['datapoints'][-1][1])
+        if lasttime + timeout > nowdate:
+            return json.dumps({'warn':'ignoring duplicate activity'})
+
     except:
-        lasttime=datetime.fromtimestamp(0)
-    if nowdate > lasttime + timeout:
         r.publish('chat',sensor)
-        return json.dumps(int(r.rpush(sensor_namespace.format(sensor),time())))
-    else:
-        #print("ignoring activity,timeout not reached")
-        return json.dumps(False)
+        return json.dumps(True)
 
 @app.route('/api/time')
 def current_time():
@@ -48,14 +72,46 @@ def current_time():
 
 @app.route('/api/sensors/<int:sensor>/last')
 def get_last_sensor_data(sensor):
-    q=r.lrange(sensor_namespace.format(sensor),-1,-1)[0]
+    return "not implemented"
+    q = data_from_graphite(sensor_namespace.format(sensor))
+    # q=r.lrange(sensor_namespace.format(sensor),-1,-1)[0]
     return json.dumps(float(q.decode()))
 
 
 @app.route('/api/sensors/<int:sensor>/all')
 def get_all_sensor_data(sensor):
-    q=r.lrange(sensor_namespace.format(sensor),0,-1)
+    return "not implemented"
+    q=data_from_graphite(sensor_namespace.format(sensor))
+    #q=r.lrange(sensor_namespace.format(sensor),0,-1)
     return json.dumps([ float(i.decode()) for i in q ])
+
+    
+
+# def event_stream(delta):
+#     # delta must be datetime.timedelta
+#     import time
+#     while True:
+#         data =[]
+#         for key in r.keys(sensor_namespace.format("*")):
+#             ts = datetime.fromtimestamp(float(r.lrange(key,-1,-1)[0] ))
+#             if ts + delta > datetime.now():
+#                 data.append(int(key.decode().split(".")[-1]))
+# 
+#         yield "data: %s\n\n" %json.dumps(data)
+#         time.sleep(5)
+# 
+# @app.route('/api/subscribe/10s')
+# def stream1():
+#     return Response(event_stream(timedelta(seconds=10)),
+#                           mimetype="text/event-stream")
+# @app.route('/api/subscribe/1m')
+# def stream2():
+#     return Response(event_stream(timedelta(minutes=1)),
+#                           mimetype="text/event-stream")
+# @app.route('/api/subscribe/10m')
+# def stream3():
+#     return Response(event_stream(timedelta(minutes=10)),
+#                           mimetype="text/event-stream")
 
 def activity_stream():
     pubsub = r.pubsub()
@@ -67,46 +123,24 @@ def activity_stream():
         else:
             print(message)
             yield 'data: %d\n\n' % int(message['data'].decode())
-    
 
-def event_stream(delta):
-    # delta must be datetime.timedelta
-    import time
-    while True:
-        data =[]
-        for key in r.keys(sensor_namespace.format("*")):
-            ts = datetime.fromtimestamp(float(r.lrange(key,-1,-1)[0] ))
-            if ts + delta > datetime.now():
-                data.append(int(key.decode().split(".")[-1]))
-
-        yield "data: %s\n\n" %json.dumps(data)
-        time.sleep(5)
-
-@app.route('/api/subscribe/10s')
-def stream1():
-    return Response(event_stream(timedelta(seconds=10)),
-                          mimetype="text/event-stream")
-@app.route('/api/subscribe/1m')
-def stream2():
-    return Response(event_stream(timedelta(minutes=1)),
-                          mimetype="text/event-stream")
-@app.route('/api/subscribe/10m')
-def stream3():
-    return Response(event_stream(timedelta(minutes=10)),
-                          mimetype="text/event-stream")
 @app.route('/api/subscribe/live')
 def livestream():
     return Response(activity_stream(),
                           mimetype="text/event-stream")
 
 def relevant_sensors():
+    return "not implemented"
     return [k.decode() for k in r.keys(rooms_namespace.format('*'))]
 
 def sensor_mapping():
+    return simple_sensor_mapping
+    """
     ret = {}
     for key in relevant_sensors():
         ret[key.split('.')[-1]] = int(r.get(key).decode())
     return ret
+    """
 
 @app.route('/api/rooms')
 def get_sensor_mapping():
@@ -114,6 +148,7 @@ def get_sensor_mapping():
 
 @app.route('/api/rooms/<name>/<int:id>')
 def set_sensor(name,id):
+    return "not implemented"
     import re
     name = re.sub('[^0-0a-zA-Z]','',name)
     return json.dumps(r.set(rooms_namespace.format(name),str(id)))
@@ -122,14 +157,30 @@ def shack_open(time):
     return True
 
 def total_seconds(sensor,begin,end):
-    q=r.lrange(sensor_namespace.format(sensor),0,-1)
+    #q=r.lrange(sensor_namespace.format(sensor),0,-1)
+    q=data_from_graphite(sensor_namespace.format(sensor),
+            {'from':begin.strftime("%H:%M_%Y%m%d"),'until':end.strftime("%H:%M_%Y%m%d")})
+
     total_time= end - begin
-    total_points=0
-    for entry in q:
-        e = datetime.fromtimestamp(float(entry))
-        if begin < e < end and shack_open(e):
-            total_points +=1
-    return total_points * point_value.seconds
+    total_secs = 0
+    try:
+        first_entry=q[0]
+    except:
+        return 0
+
+    try:
+        resolution = first_entry['datapoints'][1][1] - \
+            first_entry['datapoints'][0][1]
+    except:
+        raise Exception("not enough data points returned")
+
+    for value,timestamp in first_entry['datapoints']:
+        e = datetime.fromtimestamp(timestamp)
+        # only count if value is not None and the shack is open
+        if shack_open(e) and value:
+            # add as the seconds between the last and the current point
+            total_secs +=resolution
+    return total_secs
 
 
 @app.route('/api/stats/<int:sensor>/total/day')
@@ -141,17 +192,19 @@ def total_stats(sensor):
 @app.route('/api/stats/<int:sensor>/total')
 def total_everyday_stats(sensor):
     end=datetime.now()
-    begin=datetime.fromtimestamp(0)
+    #begin=datetime.fromtimestamp(0)
+    begin=end-timedelta(days=160)
     return json.dumps(total_seconds(sensor,begin,end))
-    #return total_seconds(sensor,
-            #datetime.now()-timedelta(hours=24),
-            #datetime.now())
+    return total_seconds(sensor,
+            datetime.now()-timedelta(hours=24),
+            datetime.now())
 
 @app.route('/api/stats')
 def total():
     ret = {}
     end=datetime.now()
-    begin=datetime.fromtimestamp(0)
+    #begin=datetime.fromtimestamp(0)
+    begin=end-timedelta(days=160)
     total_secs=0
     for name,ident in sensor_mapping().items():
         secs = int(total_seconds(ident,begin,end))
@@ -162,19 +215,17 @@ def total():
         ret[ident]['percent'] = float(ret[ident]['seconds'] /total_secs) * 100
     return json.dumps(ret)
     #return json.dumps({ "2037282": {"name":"lounge","seconds": 1234, "percent":81.3},"9846210": {"name":"kueche","seconds": 100, "percent":18.7} })
+
 @app.route('/api/temperature')
 def get_temperature():
-    import requests,json
     try:
-    r = requests.get(temperature_url)
-    c = json.loads(r.content.decode())
+        c = data_from_graphite(temperature_target)
         dp = c[0]['datapoints']
         for temperature,ts in reversed(dp):
             if temperature: 
                 return json.dumps([temperature,ts])
-    return json.dumps(['no data',0])
-
-    return json.dumps([None,None])
+    except:
+        return json.dumps(['No Data',0])
 if __name__ == '__main__':
     app.debug =True
     app.run(host='0.0.0.0',port=8888,threaded=True)
